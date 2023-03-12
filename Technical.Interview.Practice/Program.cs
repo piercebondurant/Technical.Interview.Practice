c86687badff37e912be74966caef5d81e1119959
Pierce <pierce.bondurant@gmail.com>
2023-03-11 18:21:06 -0600

    edit main for production

--- Technical.Interview.Practice/Program.cs
+++ Technical.Interview.Practice/Program.cs
@@ -11,16 +11,11 @@ namespace TechInterview.Practice
         }
         public static int MaxProductOfThree(List<int> argument)
         {
-            int size = argument.Count;
-            argument.Sort();
-            int positiveMax = argument[size - 1] * argument[size - 2] * argument[size - 3];
-            int negativeMax = argument[size - 1] * argument[0] * argument[1];
-            return Math.Max(positiveMax, negativeMax);
+            return 0;
         }
-
         static void Main(string[] args)
         {
-            new Demos.MaxProductDemo.TestMaxProduct(2, 5, 20, 10, 5).RunWithDetails();
+
         }
     }
 }
